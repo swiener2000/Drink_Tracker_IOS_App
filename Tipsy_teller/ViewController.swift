6b41f673cc0d6e957b7c11a77d7dacf0eb65f3eb
Stephanie Wiener <54724492+swiener2000@users.noreply.github.com>
2022-01-20 14:19:08 -0500

    Initial Commit

---
+++ Tipsy_teller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy_teller
+//
+//  Created by Stephanie Wiener on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
